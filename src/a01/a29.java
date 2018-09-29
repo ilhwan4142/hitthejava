5c50a2b0a20bef776d07aac30b608dd344ae172d
ikhwa <ikhwa@DESKTOP-FO7GI74>
2018-09-29 15:42:42 +0900

    29번 스태틱메소드 호출법을 사용하는 문제, ㄹㅇ 노잼

---
+++ src/a01/a29.java
@@ -0,0 +1,18 @@
+package a01;
+
+//29
+
+public class a29 {
+
+	public static final int PHYSICAL = 23;
+	
+	public static double GetVal(long days,int index,int max) {
+		return max * Math.sin((days%index) * 2 * Math.PI / index);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int days = 1200;
+		double phyval = GetVal(days,PHYSICAL,100);
+		System.out.printf("나의 신체지수 %1$.2f 입니다.\n",phyval); //$1은 첫번째 인자를 지칭한다.	
+	}
+}
