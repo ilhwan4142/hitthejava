d1dd93171b0c6feb2176f91fe80e7dbaa6fd4ad5
ikhwa <ikhwa@DESKTOP-FO7GI74>
2018-09-29 16:11:37 +0900

    31번 enum 사용방법, 기능을 배움

---
+++ src/a01/a31.java
@@ -0,0 +1,23 @@
+package a01;
+
+//31번
+
+public class a31 {
+	
+	public enum FRUIT{ //enum은 열거타입이다.
+		APPLE,BANANA,MANGO
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FRUIT pear = FRUIT.APPLE; //enum은 자료형으로 사용할 수 있으면 enum 상수 값을 이런 방식으로 초기환 한다.
+		FRUIT pear2 = FRUIT.MANGO;
+		System.out.println(pear);
+		System.out.println(pear.name());
+		System.out.println(pear.ordinal());
+		System.out.println(pear2.name());
+		System.out.println(pear2.ordinal());
+		FRUIT[] fruits = FRUIT.values();
+		System.out.println(fruits[0]);
+		System.out.println(fruits[1]);
+	}
+}
