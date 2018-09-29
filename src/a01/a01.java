a6de60db4822ee4d7426c75d20f2f25c25596dc2
ikhwa <ikhwa@DESKTOP-FO7GI74>
2018-09-29 15:31:25 +0900

    28번 $1을 배움

--- src/a01/a01.java
+++ src/a01/a01.java
@@ -1,10 +1,16 @@
 package a01;
 
+//28번
+
 public class a01 {
 
+	public static final int PHYSICAL = 23;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int index = PHYSICAL;
+		int days = 1200;
+		double phyval = 100 * Math.sin((days%index) * 2 * Math.PI / index);
+		System.out.printf("나의 신체지수 %1$.2f 입니다.",phyval); //$1은 첫번째 인자를 지칭한다.
+		
 	}
-
 }
