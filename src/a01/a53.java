992b7971e9312016487cfdd032d8498eedccc4c4
ikhwa <ikhwa@DESKTOP-FO7GI74>
2018-10-07 16:08:31 +0900

    53번 for문의 특징 중 원래 알고 있던 특징에 다시한번 다가갈 수 있는 기회가 되었다.

---
+++ src/a01/a53.java
@@ -0,0 +1,14 @@
+package a01;
+//53번
+public class a53 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] mm = {1, 2, 3, 4, 5, 6};
+		for(int m:mm) { //mm 배열에 관해 포문을 돌릴거면 이런 식으로 포문을 간소화 해서 할 수도 있다.
+			System.out.print(m+" ");
+		}
+		System.out.println();
+	}
+
+}
